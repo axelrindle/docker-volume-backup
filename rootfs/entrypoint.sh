f662eb1e50828374f58d0eec50dcb2c9656ebd0d
Axel Rindle <mail@axelrindle.de>
2022-11-26 23:35:10 +0100

    Add some output

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+set -e
+
+cat /banner.txt
+
+set -x
+
 tar c -f /backup/${ARCHIVE_NAME}.tar -C /volume .
 pigz /backup/${ARCHIVE_NAME}.tar
 chown ${UID}:${GID} /backup/${ARCHIVE_NAME}.tar.gz
+
+set +x
+
+echo
+echo "We're done here."
+echo
